31b28ddfbc53c5140f537eb93675c9bb960314c3
SmallXJM <37766687+SmallXJM@users.noreply.github.com>
2022-04-30 22:58:43 +0800

    Update user.js

--- backend/user.js
+++ backend/user.js
@@ -215,15 +215,15 @@ module.exports = class User {
       }
     } else {
       this.eid = env._id;
-      //const remarks = `remark=${this.nickName};`;
-      const body = await updateEnv(this.cookie, this.eid, "");
-      if (body.code !== 200) {
+      const remarks = `remark=${this.nickName};`;
+      const body = await updateEnv(this.cookie, this.eid, remarks);
+      /*if (body.code !== 200) {
         throw new UserError(body.message || '更新账户错误，请重试', 221, body.code || 200);
-      }
-      //this.timestamp = body.data.timestamp;
-      this.timestamp = 0;
+      }*/
+      this.timestamp = body.data.timestamp;
+      //this.timestamp = 0;
       message = `欢迎回来，${this.nickName}`;
-      //this.#sendNotify('Ninja 运行通知', `用户 ${this.nickName}(${decodeURIComponent(this.pt_pin)}) 已更新 CK`);
+      this.#sendNotify('Ninja 运行通知', `用户 ${this.nickName}(${decodeURIComponent(this.pt_pin)}) 已更新 CK`);
     }
     return {
       nickName: this.nickName,
